d68015d21f95fe894daf780662a09c49a20c1575
Arindam Kabir <arindamkabir@gmail.com>
2024-05-23 20:51:22 +0600

    feat: authorize all requests

--- app/Http/Requests/Habits/StoreHabitRequest.php
+++ app/Http/Requests/Habits/StoreHabitRequest.php
@@ -12,7 +12,7 @@ class StoreHabitRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/Habits/UpdateHabitEntryRequest.php
+++ app/Http/Requests/Habits/UpdateHabitEntryRequest.php
@@ -11,7 +11,7 @@ class UpdateHabitEntryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
