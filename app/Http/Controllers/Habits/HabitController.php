c0624deaf2faf42129a5adef5cd41a3c7a438179
Arindam Kabir <arindamkabir@gmail.com>
2024-05-23 20:02:21 +0600

    feat: add habit index route

--- app/Http/Controllers/Habits/HabitController.php
+++ app/Http/Controllers/Habits/HabitController.php
@@ -3,9 +3,30 @@
 namespace App\Http\Controllers\Habits;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\HabitResource;
+use App\Models\Habit;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HabitController extends Controller
 {
-    //
+    use ApiResponse;
+
+    public function index(Request $request)
+    {
+        $start_date = $request->input('start_date');
+        $end_date = $request->input('end_date');
+
+        if (!$start_date || !$end_date) return $this->error("No start date and/or end date found!.", 422);
+
+        $habits = Habit::query()
+            ->with(["category", "entries" => function ($query) use ($start_date, $end_date) {
+                $query->whereBetween('date', [$start_date, $end_date]);
+            }])
+            ->where('user_id', Auth::id())
+            ->get();
+
+        return HabitResource::collection($habits);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Habits\HabitController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,13 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/login', LoginController::class);
+
+Route::prefix('/habits')->name('habits.')->group(function () {
+    Route::get('/', [HabitController::class, 'index'])->name('index');
+    // Route::get('/all', [HabitController::class, 'allWithEntries'])->name('allWithEntries');
+    // Route::post('/', [HabitController::class, 'store'])->name('store');
+    // Route::post('/entries', [EntryController::class, 'save'])->name('entries.save');
+
+    // Route::get('/{slug}', [HabitController::class, 'show'])->name('show');
+    // Route::delete('/{id}', [HabitController::class, 'destroy'])->name('destroy');
+});
