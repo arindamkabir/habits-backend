f1e4cdcfaf5776939a390148d2efcc4fd28c37d6
Arindam Kabir <arindamkabir@gmail.com>
2024-05-23 19:05:24 +0600

    feat: update Habit and Habit entry models

--- app/Models/Habit.php
+++ app/Models/Habit.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
+use App\Traits\GenerateUniqueSlugTrait;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Auth;
 
 class Habit extends Model
 {
-    use HasFactory;
+    use GenerateUniqueSlugTrait, HasFactory;
+
+    // *** Relationships
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(HabitCategory::class, "category_id");
+    }
+
+    public function entries(): HasMany
+    {
+        return $this->hasMany(HabitEntry::class, "habit_id");
+    }
+
+    // *** Scopes
+
+    public function scopeCurrentUser(Builder $query): void
+    {
+        $query->where("user_id", Auth::id());
+    }
 }
--- app/Models/HabitEntry.php
+++ app/Models/HabitEntry.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
 
 class HabitEntry extends Model
 {
     use HasFactory;
+
+    // *** Relationships
+
+    public function habit(): BelongsTo
+    {
+        return $this->belongsTo(Habit::class);
+    }
+
+    // *** Scopes
+
+    public function scopeCurrentUser(Builder $query): void
+    {
+        $query->whereHas("habit", function ($query2) {
+            $query2->where("user_id", Auth::id());
+        });
+    }
 }
