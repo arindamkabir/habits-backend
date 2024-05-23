2c1095f01bf36e90f2357a156f2314b374aae355
Arindam Kabir <arindamkabir@gmail.com>
2024-05-23 19:33:05 +0600

    refactor: move habits controllers to Habits folder

--- app/Http/Controllers/Habits/HabitEntryController.php
+++ app/Http/Controllers/Habits/HabitEntryController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Habits;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class HabitEntryController extends Controller
