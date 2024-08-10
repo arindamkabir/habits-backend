09562bfee89cb3d9d47b25e323bcd5f2f7066d0b
Arindam Kabir <arindamkabir@gmail.com>
2024-08-10 07:17:32 +0600

    feat: add habit entry list function

--- app/Http/Controllers/Habits/HabitEntryController.php
+++ app/Http/Controllers/Habits/HabitEntryController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Habits;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Habits\StoreHabitEntryRequest;
+use App\Models\Habit;
 use App\Models\HabitEntry;
 use App\Services\Habits\HabitEntryService;
 use App\Traits\ApiResponse;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class HabitEntryController extends Controller
 {
@@ -20,6 +22,26 @@ class HabitEntryController extends Controller
         $this->entryService = $entryService;
     }
 
+    public function list(Request $request, string $slug)
+    {
+        if (!$request->input('start_date') || !$request->input('end_date')) {
+            return $this->error('No start date and/or end date found!.', 422);
+        }
+
+        $habit = Habit::query()
+            ->where('slug', $slug)
+            ->first();
+
+        if (!$habit || $habit->user_id !== auth()->id()) {
+            return $this->error('No habit id found!.', 422);
+        }
+
+        return $this->ok(
+            'Habit entries retrieved successfully',
+            $this->entryService->list($habit->id, $request->all())
+        );
+    }
+
     public function save(StoreHabitEntryRequest $request): JsonResponse
     {
         $validated = $request->validated();
@@ -29,7 +51,9 @@ class HabitEntryController extends Controller
             ->where('date', $validated['date'])
             ->first();
 
-        if ($entry) {
+        info($entry);
+
+        if (isset($entry)) {
             $responseEntry = $this->entryService->update($validated, $entry);
         } else {
             $responseEntry = $this->entryService->store($validated);
--- app/Services/Habits/HabitEntryService.php
+++ app/Services/Habits/HabitEntryService.php
@@ -7,9 +7,18 @@ use Illuminate\Support\Facades\DB;
 
 class HabitEntryService
 {
+    public function list(string $habitId, array $filters)
+    {
+        return HabitEntry::query()
+            ->where('habit_id', $habitId)
+            ->whereBetween('date', [$filters['start_date'], $filters['end_date']])
+            ->get();
+    }
+
     public function store(array $validated): HabitEntry
     {
         $existingEntries = HabitEntry::query()
+            ->where('habit_id', $validated['habit_id'])
             ->whereDate('date', $validated['date'])
             ->get();
 
