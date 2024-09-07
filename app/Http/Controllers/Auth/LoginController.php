fa7f7ab3fcced31c8778fe4a7e441d2b0a4807fe
Arindam Kabir <arindamkabir@gmail.com>
2024-09-07 06:40:26 +0600

    test: add log to test

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Auth\LoginRequest;
 use App\Models\User;
 use App\Traits\ApiResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class LoginController extends Controller
 {
@@ -17,6 +18,8 @@ class LoginController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
+
+        Log::info('User logged in', ['user' => $request->user()]);
 
         return $this->ok('Authenticated');
     }
